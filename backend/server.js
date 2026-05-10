4220369aec08fa3e04bc56ad5dcfabd5fff4623b
Deepak <boradeepakreddy2005@gmail.com>
2026-05-10 21:26:20 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,22 @@ import aiRoutes from "./routes/aiRoutes.js";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import profileRoutes from "./routes/profileRoutes.js"; 
+import profileRoutes from "./routes/profileRoutes.js";
 import dotenv from "dotenv";
-dotenv.config();
-
 import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/ai",aiRoutes);
+app.use("/api/ai", aiRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/auth", authRoutes);
 
-
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch((err)=>console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
-});
-
+export default app;
