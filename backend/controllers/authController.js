20c4508665a14e3f23c9a430a77aff0833aa5f68
Deepak <boradeepakreddy2005@gmail.com>
2026-05-11 15:43:26 +0530

    Updated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -199,3 +199,43 @@ export const login = async (req, res) => {
 
   }
 };
+
+export const resendOTP = async (req, res) => {
+
+  try {
+
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+
+    user.otp = otp;
+    user.otpExpires = Date.now() + 5 * 60 * 1000;
+
+    await user.save();
+
+    await sendEmail(email, otp);
+
+    res.status(200).json({
+      message: "OTP resent successfully",
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+};
