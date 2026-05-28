875c32a05e5cc1b16992365ec07d25fe23bbc3e8
Deepak <boradeepakreddy2005@gmail.com>
2026-05-28 13:05:29 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import analyticsRoutes from "./routes/analyticsRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
